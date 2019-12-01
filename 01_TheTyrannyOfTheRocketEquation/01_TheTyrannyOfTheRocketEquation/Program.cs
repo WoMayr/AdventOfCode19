19f521ef2a7e82025ba2b476309f9bc6f47b89ec
Wolfgang Mayr <wolfgangmayr798@gmail.com>
2019-12-01 13:43:09 +0100

    Added day 1

---
+++ 01_TheTyrannyOfTheRocketEquation/01_TheTyrannyOfTheRocketEquation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01_TheTyrannyOfTheRocketEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masses = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToArray();
+
+            int fuel = masses.Sum(x => CalculateFuel(x));
+
+            Console.WriteLine(fuel);
+        }
+
+
+
+        private static int CalculateFuelBase(int mass)
+        {
+            return (int)Math.Floor(mass / 3.0) - 2;
+        }
+
+        private static int CalculateFuel(int mass)
+        {
+            int total = 0;
+            int currMass = mass;
+
+            int lastFuel;
+            do
+            {
+                lastFuel = CalculateFuelBase(currMass);
+                if (lastFuel < 0)
+                {
+                    lastFuel = 0;
+                }
+                currMass = lastFuel;
+                total += lastFuel;
+            } while (lastFuel != 0);
+
+            return total;
+        }
+    }
+}
